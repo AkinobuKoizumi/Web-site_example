9349f360beb188d0015c51a13a049e5062b9d04a
EC2 Default User <ec2-user@ip-172-31-92-238.ec2.internal>
2020-08-11 11:41:39 +0000

    scroll system

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
-    (function() {
-      'use strict';
+(function() {
+  'use strict';
 
-      var openMenu = document.getElementById('open_menu');
-      var closeMenu = document.getElementById('close_menu');
-      var menu = document.getElementById('menu');
+  var openMenu = document.getElementById('open_menu');
+  var closeMenu = document.getElementById('close_menu');
+  var menu = document.getElementById('menu');
 
-      openMenu.addEventListener('click', function() {
-        menu.classList.add('shown');
-      });
+  openMenu.addEventListener('click', function() {
+    menu.classList.add('shown');
+  });
 
-      closeMenu.addEventListener('click', function() {
-        menu.classList.remove('shown');
-      });
-    })();
+  closeMenu.addEventListener('click', function() {
+    menu.classList.remove('shown');
+  });
+  
+})();
+  
+
+window.addEventListener('DOMContentLoaded', function(){
+  /** jQueryの処理 */
+  $('#back-to-top').hide();
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+          $('#back-to-top').fadeIn();
+      } else {
+          $('#back-to-top').fadeOut();
+      }
+  });
+  $('#back-to-top a').click(function() {
+      $('html, body').animate({
+          scrollTop:0
+      }, 500);
+      return false;
+  });
+});  
